47388a2639bf349a3a7119e9b399c50704837687
Hardik Solanki <52453269+HardikSolanki4@users.noreply.github.com>
2021-08-31 17:34:06 +0530

    Complete Sec-3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-function App() {
+import React from 'react';
+import Expense from './components/Expense/Expense';
+
+const App = () => {
+  const expense = [
+    {
+      title: 'car insurance',
+      amount: 255,
+      date: new Date(2020, 4, 5),
+    },
+    {
+      title: 'Bike insurance',
+      amount: 3596,
+      date: new Date(2020, 1, 9),
+    },
+    {
+      title: 'help insurance',
+      amount: 989,
+      date: new Date(2020, 8, 2),
+    },
+    {
+      title: 'food',
+      amount: 698,
+      date: new Date(2020, 9, 4),
+    },
+  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+     <Expense items={expense} />
     </div>
   );
 }
